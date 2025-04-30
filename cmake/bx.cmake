43fe1bef955a03a23f6faae75347c156d96a698f
Bret Brown <bbrown105@bloomberg.net>
2025-04-30 07:17:56 -0400

    Add FetchContent support for bx
    Problem
    -------
    
    Many Beman users desire a trivial way to enable a CMake workflow
    that clones and builds its sources from the internet.
    
    At the same time, many other Beman users want ergonomics for
    workflows in which cloning entities from the internet is never
    allowed.
    
    Solution
    --------
    
    Update `bx` and its internals to use `FetchContent` workflows by
    default, particularly through the use of `use-fetch-content.cmake`.
    In the process, `use-fetch-content.cmake` change variable prefixes
    to `Bx_*` from `BemaneExemplar_*` because those entities do not
    live in beman.exemplar any longer.
    
    Also add missing SPDX identifiers in changed files.

--- cmake/bx.cmake
+++ cmake/bx.cmake
@@ -1,3 +1,35 @@
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+cmake_minimum_required(VERSION 3.24)
+
 include_guard(GLOBAL)
 
-message(STATUS "Done bootstrapping! Write code or someting!")
+function(bx_cmake)
+    message(STATUS "Configuring FetchContent provisioning mode.")
+    set(fetch_content_shim "${bx_dir}/cmake/use-fetch-content.cmake")
+
+    set(shim_applied FALSE)
+    set(ii 3) # Skip: cmake -P bx
+    while (ii LESS CMAKE_ARGC)
+    # If -DCMAKE_PROJECT_TOP_LEVEL_INCLUDES exists, add the fetch_content_shim to it
+    if (CMAKE_ARGV${ii} MATCHES "-DCMAKE_PROJECT_TOP_LEVEL_INCLUDES=")
+        message(TRACE "Appending \"${fetch_content_shim}\" to -DCMAKE_PROJECT_TOP_LEVEL_INCLUDES.")
+        list(APPEND command_line "${CMAKE_ARGV${ii}};${fetch_content_shim}")
+        set(shim_applied TRUE)
+    else()
+        list(APPEND command_line "${CMAKE_ARGV${ii}}")
+    endif()
+    math(EXPR ii "${ii} + 1")
+    endwhile()
+
+    if(NOT shim_applied)
+    message(TRACE "Appending -DCMAKE_PROJECT_TOP_LEVEL_INCLUDES to command line.")
+    list(APPEND command_line "-DCMAKE_PROJECT_TOP_LEVEL_INCLUDES=${fetch_content_shim}")
+    endif()
+
+    # make a space delimited list of the command line arguments
+    string(REPLACE ";" " " command_line_str "${command_line}")
+    message(STATUS "Running: ${command_line_str}")
+    execute_process(COMMAND ${command_line})
+endfunction(bx_cmake)
+
+bx_cmake()
--- cmake/use-fetch-content.cmake
+++ cmake/use-fetch-content.cmake
@@ -1,175 +1,181 @@
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 cmake_minimum_required(VERSION 3.24)
 
 include(FetchContent)
 
-if(NOT BEMAN_EXEMPLAR_LOCKFILE)
-    set(BEMAN_EXEMPLAR_LOCKFILE
-        "lockfile.json"
+if(NOT BX_LOCKFILE)
+    set(BX_LOCKFILE
+        "${CMAKE_SOURCE_DIR}/bx.lockfile.json"
         CACHE FILEPATH
-        "Path to the dependency lockfile for the Beman Exemplar."
+        "Path to the dependency lockfile."
     )
 endif()
 
-set(BemanExemplar_projectDir "${CMAKE_CURRENT_LIST_DIR}/..")
-message(TRACE "BemanExemplar_projectDir=\"${BemanExemplar_projectDir}\"")
+# Set Bx_parentDir to the directory containing the lockfile
+get_filename_component(dir "${BX_LOCKFILE}" DIRECTORY)
+message(TRACE "Bx_parentDir=\"${Bx_parentDir}\"")
 
-message(TRACE "BEMAN_EXEMPLAR_LOCKFILE=\"${BEMAN_EXEMPLAR_LOCKFILE}\"")
+message(TRACE "BX_LOCKFILE=\"${BX_LOCKFILE}\"")
+# Note: Silences a deprecation warning that does not affect the behavior of
+# this code. When the cmake_minimum_required version is set to 3.24 or higher,
+# the following line can be harmlessly deleted.
+cmake_policy(SET CMP0152 OLD)
 file(
     REAL_PATH
-    "${BEMAN_EXEMPLAR_LOCKFILE}"
-    BemanExemplar_lockfile
-    BASE_DIRECTORY "${BemanExemplar_projectDir}"
+    "${BX_LOCKFILE}"
+    Bx_lockfile
+    BASE_DIRECTORY "${Bx_parentDir}"
     EXPAND_TILDE
 )
-message(DEBUG "Using lockfile: \"${BemanExemplar_lockfile}\"")
+message(DEBUG "Using lockfile: \"${Bx_lockfile}\"")
 
 # Force CMake to reconfigure the project if the lockfile changes
 set_property(
-    DIRECTORY "${BemanExemplar_projectDir}"
+    DIRECTORY "${Bx_parentDir}"
     APPEND
-    PROPERTY CMAKE_CONFIGURE_DEPENDS "${BemanExemplar_lockfile}"
+    PROPERTY CMAKE_CONFIGURE_DEPENDS "${Bx_lockfile}"
 )
 
 # For more on the protocol for this function, see:
 # https://cmake.org/cmake/help/latest/command/cmake_language.html#provider-commands
-function(BemanExemplar_provideDependency method package_name)
+function(Bx_provideDependency method package_name)
     # Read the lockfile
-    file(READ "${BemanExemplar_lockfile}" BemanExemplar_rootObj)
+    file(READ "${Bx_lockfile}" Bx_rootObj)
 
-    # Get the "dependencies" field and store it in BemanExemplar_dependenciesObj
+    # Get the "dependencies" field and store it in Bx_dependenciesObj
     string(
         JSON
-        BemanExemplar_dependenciesObj
-        ERROR_VARIABLE BemanExemplar_error
-        GET "${BemanExemplar_rootObj}"
+        Bx_dependenciesObj
+        ERROR_VARIABLE Bx_error
+        GET "${Bx_rootObj}"
         "dependencies"
     )
-    if(BemanExemplar_error)
-        message(FATAL_ERROR "${BemanExemplar_lockfile}: ${BemanExemplar_error}")
+    if(Bx_error)
+        message(FATAL_ERROR "${Bx_lockfile}: ${Bx_error}")
     endif()
 
-    # Get the length of the libraries array and store it in BemanExemplar_dependenciesObj
+    # Get the length of the libraries array and store it in Bx_dependenciesObj
     string(
         JSON
-        BemanExemplar_numDependencies
-        ERROR_VARIABLE BemanExemplar_error
-        LENGTH "${BemanExemplar_dependenciesObj}"
+        Bx_numDependencies
+        ERROR_VARIABLE Bx_error
+        LENGTH "${Bx_dependenciesObj}"
     )
-    if(BemanExemplar_error)
-        message(FATAL_ERROR "${BemanExemplar_lockfile}: ${BemanExemplar_error}")
+    if(Bx_error)
+        message(FATAL_ERROR "${Bx_lockfile}: ${Bx_error}")
     endif()
 
     # Loop over each dependency object
-    math(EXPR BemanExemplar_maxIndex "${BemanExemplar_numDependencies} - 1")
-    foreach(BemanExemplar_index RANGE "${BemanExemplar_maxIndex}")
-        set(BemanExemplar_errorPrefix
-            "${BemanExemplar_lockfile}, dependency ${BemanExemplar_index}"
+    math(EXPR Bx_maxIndex "${Bx_numDependencies} - 1")
+    foreach(Bx_index RANGE "${Bx_maxIndex}")
+        set(Bx_errorPrefix
+            "${Bx_lockfile}, dependency ${Bx_index}"
         )
 
-        # Get the dependency object at BemanExemplar_index
-        # and store it in BemanExemplar_depObj
+        # Get the dependency object at Bx_index
+        # and store it in Bx_depObj
         string(
             JSON
-            BemanExemplar_depObj
-            ERROR_VARIABLE BemanExemplar_error
-            GET "${BemanExemplar_dependenciesObj}"
-            "${BemanExemplar_index}"
+            Bx_depObj
+            ERROR_VARIABLE Bx_error
+            GET "${Bx_dependenciesObj}"
+            "${Bx_index}"
         )
-        if(BemanExemplar_error)
+        if(Bx_error)
             message(
                 FATAL_ERROR
-                "${BemanExemplar_errorPrefix}: ${BemanExemplar_error}"
+                "${Bx_errorPrefix}: ${Bx_error}"
             )
         endif()
 
-        # Get the "name" field and store it in BemanExemplar_name
+        # Get the "name" field and store it in Bx_name
         string(
             JSON
-            BemanExemplar_name
-            ERROR_VARIABLE BemanExemplar_error
-            GET "${BemanExemplar_depObj}"
+            Bx_name
+            ERROR_VARIABLE Bx_error
+            GET "${Bx_depObj}"
             "name"
         )
-        if(BemanExemplar_error)
+        if(Bx_error)
             message(
                 FATAL_ERROR
-                "${BemanExemplar_errorPrefix}: ${BemanExemplar_error}"
+                "${Bx_errorPrefix}: ${Bx_error}"
             )
         endif()
 
-        # Get the "package_name" field and store it in BemanExemplar_pkgName
+        # Get the "package_name" field and store it in Bx_pkgName
         string(
             JSON
-            BemanExemplar_pkgName
-            ERROR_VARIABLE BemanExemplar_error
-            GET "${BemanExemplar_depObj}"
+            Bx_pkgName
+            ERROR_VARIABLE Bx_error
+            GET "${Bx_depObj}"
             "package_name"
         )
-        if(BemanExemplar_error)
+        if(Bx_error)
             message(
                 FATAL_ERROR
-                "${BemanExemplar_errorPrefix}: ${BemanExemplar_error}"
+                "${Bx_errorPrefix}: ${Bx_error}"
             )
         endif()
 
-        # Get the "git_repository" field and store it in BemanExemplar_repo
+        # Get the "git_repository" field and store it in Bx_repo
         string(
             JSON
-            BemanExemplar_repo
-            ERROR_VARIABLE BemanExemplar_error
-            GET "${BemanExemplar_depObj}"
+            Bx_repo
+            ERROR_VARIABLE Bx_error
+            GET "${Bx_depObj}"
             "git_repository"
         )
-        if(BemanExemplar_error)
+        if(Bx_error)
             message(
                 FATAL_ERROR
-                "${BemanExemplar_errorPrefix}: ${BemanExemplar_error}"
+                "${Bx_errorPrefix}: ${Bx_error}"
             )
         endif()
 
-        # Get the "git_tag" field and store it in BemanExemplar_tag
+        # Get the "git_tag" field and store it in Bx_tag
         string(
             JSON
-            BemanExemplar_tag
-            ERROR_VARIABLE BemanExemplar_error
-            GET "${BemanExemplar_depObj}"
+            Bx_tag
+            ERROR_VARIABLE Bx_error
+            GET "${Bx_depObj}"
             "git_tag"
         )
-        if(BemanExemplar_error)
+        if(Bx_error)
             message(
                 FATAL_ERROR
-                "${BemanExemplar_errorPrefix}: ${BemanExemplar_error}"
+                "${Bx_errorPrefix}: ${Bx_error}"
             )
         endif()
 
         if(method STREQUAL "FIND_PACKAGE")
-            if(package_name STREQUAL BemanExemplar_pkgName)
+            if(package_name STREQUAL Bx_pkgName)
                 string(
                     APPEND
-                    BemanExemplar_debug
-                    "Redirecting find_package calls for ${BemanExemplar_pkgName} "
-                    "to FetchContent logic fetching ${BemanExemplar_repo} at "
-                    "${BemanExemplar_tag} according to ${BemanExemplar_lockfile}."
+                    Bx_debug
+                    "Redirecting find_package calls for ${Bx_pkgName} "
+                    "to FetchContent logic fetching ${Bx_repo} at "
+                    "${Bx_tag} according to ${Bx_lockfile}."
                 )
-                message(DEBUG "${BemanExemplar_debug}")
+                message(DEBUG "${Bx_debug}")
                 FetchContent_Declare(
-                    "${BemanExemplar_name}"
-                    GIT_REPOSITORY "${BemanExemplar_repo}"
-                    GIT_TAG "${BemanExemplar_tag}"
+                    "${Bx_name}"
+                    GIT_REPOSITORY "${Bx_repo}"
+                    GIT_TAG "${Bx_tag}"
                     EXCLUDE_FROM_ALL
                 )
                 set(INSTALL_GTEST OFF) # Disable GoogleTest installation
-                FetchContent_MakeAvailable("${BemanExemplar_name}")
+                FetchContent_MakeAvailable("${Bx_name}")
 
                 # Important! <PackageName>_FOUND tells CMake that `find_package` is
                 # not needed for this package anymore
-                set("${BemanExemplar_pkgName}_FOUND" TRUE PARENT_SCOPE)
+                set("${Bx_pkgName}_FOUND" TRUE PARENT_SCOPE)
             endif()
         endif()
     endforeach()
 endfunction()
 
 cmake_language(
-    SET_DEPENDENCY_PROVIDER BemanExemplar_provideDependency
+    SET_DEPENDENCY_PROVIDER Bx_provideDependency
     SUPPORTED_METHODS FIND_PACKAGE
 )
